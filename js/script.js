1c2013a434374b2efb5892c1fdc1f7d1c48953a0
Disa0808 <levshukdenis@mail.ru>
2018-11-04 00:45:41 +0300

    feat: complete comments block

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+  $('#carousel').owlCarousel({
+      loop:true,
+      nav:false, 
+      smartSpeed:500, 
+      autoplayTimeout:4000,
+      items:1
+  });
+  
+});
